9ed6e0649bf774c5627e5e330267ef447dfa9d0c
Abinusawa <aopeyemi906@gmail.com>
2022-06-09 07:50:46 -0700

    FunctionsNestedLoops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: 0
- *     */
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
-	char p[] = "_putchar";
-	int i;
+	char text[9] = "_putchar";
+	int i = 0;
 
 	for (i = 0; i < 8; i++)
 	{
-	_putchar(p[i]);
+		_putchar(text[i]);
 	}
 	_putchar('\n');
+
 	return (0);
 }
