7de877f12e344078579c646f7cb35bd41d1ff03a
Abinusawa <aopeyemi906@gmail.com>
2022-06-09 03:26:16 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-		char p[] = "_putchar";
-			int i;
+	char p[] = "_putchar";
+	int i;
 
-				for (i = 0; i < 8; i++)
-						{
-								_putchar(p[i]);
-									}
-					_putchar('\n');
-						return (0);
+	for (i = 0; i < 8; i++)
+	{
+	_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
